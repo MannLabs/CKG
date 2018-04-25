ee1e99b38fbfc6c72ed673798de261e9ae6f4bae
Alberto <alb.santosdel@gmail.com>
2018-04-25 18:12:29 +0200

    Added a more complex heatmap with a dendrogram

--- KnowledgeViewer/initialApp.py
+++ KnowledgeViewer/initialApp.py
@@ -2,8 +2,11 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
+from plotly.graph_objs import *
+import plotly.figure_factory as FF
 import pandas as pd
 import numpy as np
+from scipy.spatial.distance import pdist, squareform
 
 app = dash.Dash()
 
@@ -82,6 +85,78 @@ def getHeatmapFigure(data, identifier, title):
                                     y = list(data.index)))
     return dcc.Graph(id = identifier, figure = figure)
 
+def getComplexHeatmapFigure(data, identifier, title):
+    figure = FF.create_dendrogram(data.values, orientation='bottom', labels=list(data.columns))
+    for i in range(len(figure['data'])):
+        figure['data'][i]['yaxis'] = 'y2'
+    dendro_side = FF.create_dendrogram(data.values, orientation='right')
+    for i in range(len(dendro_side['data'])):
+        dendro_side['data'][i]['xaxis'] = 'x2'
+
+    # Add Side Dendrogram Data to Figure
+    figure['data'].extend(dendro_side['data'])
+
+    dendro_leaves = dendro_side['layout']['yaxis']['ticktext']
+    dendro_leaves = list(map(int, dendro_leaves))
+    data_dist = pdist(data.values)
+    heat_data = squareform(data_dist)
+    heat_data = heat_data[dendro_leaves,:]
+    heat_data = heat_data[:,dendro_leaves]
+
+    heatmap = Data([
+        Heatmap(
+            x = dendro_leaves,
+            y = dendro_leaves,
+            z = heat_data,
+            colorscale = 'YIGnBu'
+        )
+    ])
+
+    heatmap[0]['x'] = figure['layout']['xaxis']['tickvals']
+    heatmap[0]['y'] = dendro_side['layout']['yaxis']['tickvals']
+
+    # Add Heatmap Data to Figure
+    figure['data'].extend(Data(heatmap))
+
+    # Edit Layout
+    figure['layout'].update({'width':800, 'height':800,
+                             'showlegend':False, 'hovermode': 'closest',
+                             })
+    # Edit xaxis
+    figure['layout']['xaxis'].update({'domain': [.15, 1],
+                                      'mirror': False,
+                                      'showgrid': False,
+                                      'showline': False,
+                                      'zeroline': False,
+                                      'ticks':""})
+    # Edit xaxis2
+    figure['layout'].update({'xaxis2': {'domain': [0, .15],
+                                       'mirror': False,
+                                   'showgrid': False,
+                                   'showline': False,
+                                   'zeroline': False,
+                                   'showticklabels': False,
+                                   'ticks':""}})
+
+    # Edit yaxis
+    figure['layout']['yaxis'].update({'domain': [0, .85],
+                                      'mirror': False,
+                                      'showgrid': False,
+                                      'showline': False,
+                                      'zeroline': False,
+                                      'showticklabels': False,
+                                      'ticks': ""})
+    # Edit yaxis2
+    figure['layout'].update({'yaxis2':{'domain':[.825, .975],
+                                       'mirror': False,
+                                       'showgrid': False,
+                                       'showline': False,
+                                       'zeroline': False,
+                                       'showticklabels': False,
+                                       'ticks':""}})
+
+    return dcc.Graph(id = identifier, figure = figure)
+
 def appendToDashLayout(data, layout):
     return layout.append(data)
 
@@ -102,10 +177,13 @@ if __name__ == '__main__':
     data = pd.DataFrame([("Protein 1", 1, 2), ("Protein 2", 1, 3), ("Protein 3", 2, 0.5), ("Protein 4",2 ,4)], columns = ["name", "AS1", "AS2"])
     figure2 = getScatterPlotFigure(data, identifier= "myPlot2", x = "AS1", y = "AS2", x_title = "Analytical Sample 1", y_title = "Analytical Sample 2", title = "Correlation Analytical Samples")
     layout.append(figure2)
-    data = pd.DataFrame([("Protein 1", 1, 2, 2, 5), ("Protein 2", 1, 3, 3, 3), ("Protein 3", 2, 0.5, 0), ("Protein 4",2 ,4, 10, 20)], columns = ["name", "AS1", "AS2", "AS3", "AS4"])
+    data = pd.DataFrame([("Protein 1", 1, 2, 2, 5), ("Protein 2", 1, 3, 3, 3), ("Protein 3", 2, 0.5, 0, 0.8), ("Protein 4",2 ,4, 10, 20)], columns = ["name", "AS1", "AS2", "AS3", "AS4"])
     data = data.set_index("name")
     figure3 = getHeatmapFigure(data, identifier = "Heat", title = "What a heatmap!")
     layout.append(figure3)
+    figure4 = getComplexHeatmapFigure(data, identifier = "Heat 2", title = "WHHHHHHA")
+    layout.append(figure4)
+
     layoutData(layout)
 
     app.run_server(debug=True)
