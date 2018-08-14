711756abf21fa439d8b25848e6e1afed34dfefa1
Alberto <alb.santosdel@gmail.com>
2018-08-14 17:23:46 +0200

    Creating a proteomicsDataset class that will take care of processing, analyses and plots for Proteomics

--- KnowledgeMiner/production/Project.py
+++ KnowledgeMiner/production/Project.py
@@ -1,10 +1,13 @@
-
+import project_config as config
+import project_cypher as cypher
 
 class Project:
     def __init__(identifier, project_type, datasets = []):
         self.identifier = identifier
         self.project_type = project_type
         self.datasets = datasets
+        if len(datasets) == 0:
+            self.buildProject()
 
     def getIdentifier(self):
         return self.identifier
@@ -24,6 +27,66 @@ class Project:
     def setDatasets(self, datasets):
         self.datasets = datasets
 
-    def generateReport(self):
-        pass
+    def buildProject(self):
+        for dataset_type in config:
+            if dataset_type == "proteomics":
+                proteomicsDataset = ProteomicsDataset(self.getIdentifier(), config[dataset_type])
+
+
+
+            for section in config[key]:
+                for section_query,analysis_types,plot_names,args in config[key][section]:
+                    args["id"] = self.getProjectId()
+                    plots = viewer.view(key, section_query, analysis_types, plot_names, args)
+                    self.extendLayout(plots)
+
+    def generateReport(self, configuration):
+        for dataset in self.getDatasets():
+            if dataset.getType() in configuration: 
+                dataset.generateReport(configuration[dataset.getType()])
+    
+class ProteomicsDataset:
+    def __init__(identifier, configuration, data={}):
+        self.identifier = identifier
+        self.type = "proteomics"
+        self.configuration = configuration
+        self.data = data
+        if len(data) == 0:
+            self.queryData()
+
+    def getIdentifier(self):
+        return self.identifier
+
+    def getType(self):
+        return self.type
+
+    def getConfiguration(self):
+        return self.configuration
+
+    def setIdentifier(self, identifier):
+        self.identifier = identifier
+
+    def setType(self):
+        self.type = "proteomics"
+
+    def setConfiguration(self, configuration):
+        self.configuration  = configuration
+
+    def preprocessData(data, args):
+        imputation = True
+        method = "mixed"
+        missing_method = 'percentage'
+        missing_max = 0.3
+        
+        if "imputation" in args:
+            imputation = args["imputation"]
+        if "imputation_method" in args:
+            method = args["imputation_method"]
+        if "missing_method" in args:
+            missing_method = args["missing_method"]
+        if "missing_max" in args:
+            missing_max = args["missing_max"]
+        
+        data = analyses.get_measurements_ready(data, imputation = imputation, method = method, missing_method = missing_method, missing_max = missing_max)
+    return data
 
