d24536fb68f471927f1c70a38c7604f72a72aca7
Alberto <alb.santosdel@gmail.com>
2018-11-24 13:53:33 +0100

    Removed unused cypher queries

--- report_manager/queries/proteomics_cypher.py
+++
@@ -1,8 +0,0 @@
-queries = {
-    "NUMBER_PROTEINS_ANALYTICAL_SAMPLE":("Number of Proteins", 
-            '''MATCH p=(project:Project)-[*3]-(a:Analytical_sample)-[r:HAS_QUANTIFIED_PROTEIN]->(protein:Protein) WHERE project.id="PROJECTID" RETURN a.id,a.group,protein.id,r.value;'''),
-    "NUMBER_PEPTIDES_ANALYTICAL_SAMPLE":("Number of Peptides", 
-            '''MATCH p=(project:Project)-[*3]-(a:Analytical_sample)-[r:HAS_QUANTIFIED_PEPTIDE]->(peptide:Peptide) WHERE project.id="PROJECTID" RETURN a.id,a.group,peptide.id,r.value;'''),
-    "NUMBER_MODIFIED_PROTEINS_ANALYTICAL_SAMPLE":("Number of Modified Proteins", 
-            '''MATCH p=(project:Project)-[*3]-(a:Analytical_sample)-[r:HAS_QUANTIFIED_MODIFIED_PROTEIN]->(modifiedprotein:Modified_protein) WHERE project.id="P0000001" RETURN a.id,a.group,modifiedprotein.id,r.value;''')
-}
