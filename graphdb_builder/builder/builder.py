4924d34c963008c1e5dce0b89521b79be385d0bf
Alberto <alb.santosdel@gmail.com>
2019-01-08 17:39:42 +0100

    Change in the cypher that loads stitch data and correction of how builder reads list arguments

--- graphdb_builder/builder/builder.py
+++ graphdb_builder/builder/builder.py
@@ -30,9 +30,9 @@ except Exception as err:
 def set_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("-b", "--build_type", help="define the type of build you want (import, load or both)", type=str, choices=['import', 'load', 'full'], default='full')
-    parser.add_argument("-i", "--import_types", help="define which data types (ontologies, experiments, databases) you want to import (partial import)", type=list, default=None, choices=['experiments', 'databases', 'ontologies'])
-    parser.add_argument("-l", "--load_entities",  help="define which entities you want to load into the database (partial load)", type=list, default=config["graph"])
-    parser.add_argument("-d", "--data", help="define which ontology/ies, experiment/s or database/s you want to import", type=list, default=None)
+    parser.add_argument("-i", "--import_types", help="define which data types (ontologies, experiments, databases) you want to import (partial import)", nargs='+', default=None, choices=['experiments', 'databases', 'ontologies'])
+    parser.add_argument("-l", "--load_entities",  help="define which entities you want to load into the database (partial load)",  nargs='+', default=config["graph"])
+    parser.add_argument("-d", "--data", help="define which ontology/ies, experiment/s or database/s you want to import",  nargs='+', default=None)
     parser.add_argument("-n", "--n_jobs", help="define number of cores used when importing data", type=int, default=4)
     parser.add_argument("-w", "--download", help="define whether or not to download imported data", type=bool, default=True)
     parser.add_argument("-u", "--user", help="define the user that will build the database", type=str, required=True)
@@ -52,27 +52,27 @@ if __name__ == '__main__':
     elif args.build_type == 'import':
         logger.info("The user chose to perform a partial build")
         if args.import_types is not None:
-            if len(args.data) > 1:
+            if len(args.data) > 0:
                 logger.info("The build will import data from {}".format("".join(args.import_types)))
                 for import_type in args.import_types:
                     logger.info("Importing {}: {}".format(import_type, "".join(args.data)))
                     if import_type.lower() == 'experiments' or import_type.lower() == 'experiment':
-                        experimentsImport(projects=args.data, n_jobs=1)
+                        importer.experimentsImport(projects=args.data, n_jobs=1)
                     elif import_type.lower() == 'databases' or import_type.lower() == 'database':
-                        valid_entities = set([x.lower() for x in dbconfig['databases']]).intersect([x.lower() for x in args.data])
+                        valid_entities = set([x.lower() for x in dbconfig['databases']]).intersection([x.lower() for x in args.data])
                         if len(valid_entities) > 0:
                             logger.info("These entities will be imported: {}".format(", ".join(valid_entities)))
                             print("These entities will be loaded into the database: {}".format(", ".join(valid_entities)))
-                            databasesImport(importDirectory='../../../data/imports', databases=valid_entities, n_jobs=args.n_jobs, download=args.download)
+                            importer.databasesImport(importDirectory='../../../data/imports', databases=valid_entities, n_jobs=args.n_jobs, download=args.download)
                         else:
                             logger.error("The indicated entities (--data) cannot be imported: {}".format(args.data))
                             print("The indicated entities (--data) cannot be imported: {}".format(args.data))
                     elif import_type.lower() == 'ontologies' or import_type.lower() == 'ontology':
-                        valid_entities = set([x.lower() for x in oconfig['ontologies']]).intersect([x.lower() for x in args.data])
+                        valid_entities = set([x.lower() for x in oconfig['ontologies']]).intersection([x.lower() for x in args.data])
                         if len(valid_entities) > 0:
                             logger.info("These entities will be imported: {}".format(", ".join(valid_entities)))
                             print("These entities will be loaded into the database: {}".format(", ".join(valid_entities)))
-                            ontologiesImport(importDirectory='../../../data/imports', ontologies=valid_entities)
+                            importer.ontologiesImport(importDirectory='../../../data/imports', ontologies=valid_entities)
                         else:
                             logger.error("The indicated entities (--data) cannot be imported: {}".format(args.data))
                             print("The indicated entities (--data) cannot be imported: {}".format(args.data))
@@ -82,7 +82,7 @@ if __name__ == '__main__':
     elif args.build_type == 'load':
         logger.info("The build will load data into the database: {}".format("".join(args.load_entities)))
         if len(args.load_entities) > 0:
-            valid_entities = set([x.lower() for x in config['graph']]).intersect([x.lower() for x in args.load_entities])
+            valid_entities = set([x.lower() for x in config['graph']]).intersection([x.lower() for x in args.load_entities])
 
             if len(valid_entities) > 0:
                 logger.info("These entities will be loaded into the database: {}".format(", ".join(valid_entities)))
