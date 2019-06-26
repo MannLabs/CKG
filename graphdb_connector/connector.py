fc52992c3c9319b16540f8863b4d82f9ac52a69f
Rita Colaço <rita.colaco@cpr.ku.dk>
2019-06-26 19:03:19 +0200

    Making a safe copy to master

--- graphdb_connector/connector.py
+++ graphdb_connector/connector.py
@@ -2,13 +2,30 @@ import sys
 import os
 import py2neo
 import pandas as pd
-from graphdb_connector import connector_config as config
+import ckg_utils
+from config import ckg_config
+from graphdb_builder import builder_utils
 
-def getGraphDatabaseConnectionConfiguration():
-    host = config.dbURL
-    port = config.dbPort
-    user = config.dbUser
-    password = config.dbPassword
+import logging
+import logging.config
+
+log_config = ckg_config.graphdb_connector_log
+logger = builder_utils.setup_logging(log_config, key="connector")
+
+try:
+    cwd = os.path.abspath(os.path.dirname(__file__))
+    path = os.path.join(cwd, ckg_config.connector_config_file)
+    config = ckg_utils.get_configuration(path)
+except Exception as err:
+    logger.error("Reading configuration > {}.".format(err))
+
+def getGraphDatabaseConnectionConfiguration(configuration=None):
+    if configuration is None:
+        configuration = config
+    host = configuration['db_url']
+    port = configuration['db_port']
+    user = configuration['db_user']
+    password = configuration['db_password']
 
     driver = connectToDB(host, port, user, password)
 
@@ -40,17 +57,37 @@ def removeRelationshipDB(entity1, entity2, relationship):
     deleteCy = cy.REMOVE_RELATIONSHIPS
     countst = countCy.replace('ENTITY1', entity1).replace('ENTITY2', entity2).replace('RELATIONSHIP', relationship)
     deletest = deleteCy.replace('ENTITY1', entity1).replace('ENTITY2', entity2).replace('RELATIONSHIP', relationship)
-    print(countst)
-    print(sendQuery(driver, countst).data()[0])
     print("Removing %d entries in the database" % sendQuery(driver, countst).data()[0]['count'])
     sendQuery(driver, deletest)
     print("Existing entries after deletion: %d" % sendQuery(driver, countst).data()[0]['count'])
 
-def sendQuery(driver, query):
+def modifyEntityProperty(parameters):
+    '''parameters: tuple with entity name, entity id, property name to modify, and value'''
+
+    driver = connector.getGraphDatabaseConnectionConfiguration()
+    entity, entityid, attribute, value = parameters
+
+    try:
+        cwd = os.path.abspath(os.path.dirname(__file__))
+        queries_path = "./queries.yml"
+        project_cypher = ckg_utils.get_queries(os.path.join(cwd, queries_path))
+        for query_name in project_cypher:
+            title = query_name.lower().replace('_',' ')
+            if title == 'modify':
+                query = project_cypher[query_name]['query'] % (entity, entityid, attribute, value)
+                sendQuery(driver, query)
+                print("Property successfully modified")
+    except Exception as err:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            logger.error("Reading queries from file {}: {}, file: {},line: {}".format(queries_path, sys.exc_info(), fname, exc_tb.tb_lineno))
+
+
+def sendQuery(driver, query, parameters={}):
     #print(query)
     result = None
     try:
-        result = driver.run(query)
+        result = driver.run(query, parameters)
     except py2neo.database.DatabaseError as err:
         raise py2neo.database.DatabaseError("Database failed to service the request. {}".format(err))
     except py2neo.database.ClientError as err:
@@ -67,8 +104,8 @@ def sendQuery(driver, query):
 
     return result
 
-def getCursorData(driver, query):
-    result = sendQuery(driver, query)
+def getCursorData(driver, query, parameters={}):
+    result = sendQuery(driver, query, parameters)
     df = pd.DataFrame(result.data())
-    
+
     return df
