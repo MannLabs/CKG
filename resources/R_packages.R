9c84bb0aa219828e12b6a03a96c713f0d022ae55
Alberto <alb.santosdel@gmail.com>
2020-06-30 06:42:20 +0200

    Making Docker work with OpenJDK and solving permission denied errors

--- resources/R_packages.R
+++ resources/R_packages.R
@@ -1,4 +1,5 @@
 install.packages('BiocManager')
 BiocManager::install()
 BiocManager::install(c('AnnotationDbi', 'GO.db', 'preprocessCore', 'impute'))
-install.packages(c('flashClust','WGCNA', 'samr'), dependencies=TRUE, repos='http://cran.rstudio.com/')
+install.packages(c('devtools', 'tidyverse', 'flashClust', 'WGCNA', 'samr'), dependencies=TRUE, repos='http://cran.rstudio.com/')
+install.packages('IRkernel')
