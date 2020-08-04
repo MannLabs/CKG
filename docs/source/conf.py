dce801e77d0175567a9d9aed65958f577b0a4f5f
Rita Colaço <rita.colaco@cpr.ku.dk>
2020-08-04 14:34:42 +0200

    Removed pandas from docs requirements and fixed minor errors in documentation.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ import sphinx_rtd_theme
 
 sys.path.insert(0, os.path.abspath( './../..'))
 
-MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate', 'pandas.compat.numpy']
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
