d8bdcb79683d228fceea94e74c01c500b9ccc6f8
Rita Colaço <rita.colaco@cpr.ku.dk>
2020-08-04 09:26:10 +0200

    Fixed minor error in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,16 +12,14 @@
 #
 import os
 import sys
+import mock
 import sphinx_rtd_theme
 
 sys.path.insert(0, os.path.abspath( './../..'))
 
-
-import mock
-
 MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate']
 for mod_name in MOCK_MODULES:
-sys.modules[mod_name] = mock.Mock()
+    sys.modules[mod_name] = mock.Mock()
 
 # -- Project information -----------------------------------------------------
 
