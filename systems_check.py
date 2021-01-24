445a450ec177d04d04146cc5fe6993bec7514328
Maximilian Strauss <straussmaximilian@gmail.com>
2021-01-24 16:26:45 +0100

    ADD systems_check_notebook

---
+++ systems_check.py
@@ -0,0 +1,15 @@
+import subprocess
+#Script to check if all requirements are installed
+to_check = {}
+to_check['java'] = (['Java', '-version'], None) #command, desired output
+to_check['R'] = (['R', '--version'], None)
+to_check['R path'] = (['which', 'R'], '/usr/local/bin/R')
+
+for _ in to_check:
+    try:
+        out = subprocess.check_output(to_check[_][0], stderr=subprocess.STDOUT).decode("utf-8").rstrip()
+        if to_check[_][1] is not None:
+            assert to_check[_][1] == out
+        print(f"{'='*10} Checking {_} {'='*10} \n {out}")
+    except FileNotFoundError as e:
+        print(f"Error checking {_}. Error: {e}.")
