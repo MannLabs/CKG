51b1766bac1f1e58516109b6584ebf7251a46e70
Alberto Santos <alb.santosdel@gmail.com>
2021-03-17 11:21:29 +0100

    Modified setup to install requirements and generate entry point

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
+import pathlib
 import setuptools
 from setuptools import setup
+from setuptools.command.install import install
+import pkg_resources
 
+with pathlib.Path('requirements.txt').open() as requirements_txt:
+    reqs = [
+        str(requirement)
+        for requirement
+        in pkg_resources.parse_requirements(requirements_txt)
+    ]
+    
 setuptools.setup(
     name="CKG", # Replace with your own username
     version="1.0.0",
@@ -9,11 +19,16 @@ setuptools.setup(
     description="A Python project that allows you to analyse proteomics and clinical data, and integrate and mine knowledge from multiple biomedical databases widely used nowadays.",
     url="https://github.com/MannLabs/CKG",
     packages=setuptools.find_packages(),
+    install_requires=reqs,
+    entry_points='''
+        [console_scripts]
+        ckg_app=ckg.report_manager.index:main
+    ''',
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='==3.7.9',
     include_package_data=True,
 )
