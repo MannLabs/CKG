6749dad3fec89a2c60dcaeacdf6bd22aa37baef2
Alberto Santos <alb.santosdel@gmail.com>
2021-06-14 10:13:47 +0200

    Minor changes to clean up the docker_entrypoint file

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
 echo "Entry point to CKG Docker"
-echo $DUMP_PATH
-echo $EXEC_MODE
-
 cd /CKG
 
 echo "Starting Neo4j"
 service neo4j start &
 service neo4j status
 
-#wget --quiet --tries=10 --waitretry=32 -O /dev/null http://localhost:7474
 while ! [[ `wget -S --spider http://localhost:7474  2>&1 | grep 'HTTP/1.1 200 OK'` ]]; do
 echo "Database not ready"
 sleep 60
 done
 
 echo "Database ready"
-
 echo "Creating Test user in the database"
 python3 ckg/graphdb_builder/builder/create_user.py -u test_user -d test_user -n test -e test@ckg.com -a test
-
 
 echo "Running jupyterHub"
 jupyterhub -f /etc/jupyterhub/jupyterhub.py --no-ssl &
@@ -33,6 +27,6 @@ celery -A ckg.report_manager.worker worker --loglevel=INFO --concurrency=1 -E -Q
 celery -A ckg.report_manager.worker worker --loglevel=INFO --concurrency=3 -E -Q compute &
 celery -A ckg.report_manager.worker worker --loglevel=INFO --concurrency=1 -E -Q update &
 
-cd /CKG
 echo "Initiating CKG app"
+cd /CKG
 nginx && uwsgi --ini /etc/uwsgi/apps-enabled/uwsgi.ini --uid 1500 --gid nginx
